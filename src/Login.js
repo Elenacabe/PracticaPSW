0b61c8e32e56f7addc2c20c92d5684871fdc4f7b
Elena Ceinos <elenaceinos@hotmail.com>
2024-10-15 13:08:14 +0200

    Added error message

--- src/Login.js
+++ src/Login.js
@@ -1,28 +1,39 @@
 import React, { useState } from 'react';
+import './Login.css';
 
 function Login({ onLogin }) {
   const [username, setUsername] = useState('');
 
+  const [error, setError] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (username) {
+    if (username.length > 13) {
+      setError('No puedes iniciar sesión con más de 13 caracteres');
+    } else if (username) {
+      setError('');
       onLogin(username);
     }
   };
 
   return (
-    <div className="login-container">
-      <h2>Indica tu nombre</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Nombre de usuario"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button type="submit">Jugar</button>
-      </form>
-    </div>
+    <div className='bodylog'>
+      <div className="login-container">
+        <h2>Indica tu nombre</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Nombre de usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button type="submit">Jugar</button>
+        </form>
+      </div>
+      <br/>
+        {error && <p className='not-succeed'>{error}</p>}
+      
+  </div>
   );
 }
 
