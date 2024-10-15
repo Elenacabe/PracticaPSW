9574533bce675468cab0ed42cd8b7d388618c442
Elena Ceinos <elenaceinos@hotmail.com>
2024-10-15 15:10:56 +0200

    error message, footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,16 @@
-import React, { memo } from 'react';
-import logo_empresa from './assets/logo_empresa.jpeg'
-import './footer.css'
+import React from 'react';
+import logo_empresa from './assets/logo_empresa.jpeg';
+import './footer.css';
 
-
-const Footer = memo(() => {
+const Footer = () => {
   return (
     <footer>
-     <div className="footer-container">
-                <p>Created by:</p><br/>
-                <img className='logo' src={logo_empresa} alt='logo'></img>
-            </div>
+      <div className="footer-container">
+        <p>Created by:</p><br/>
+        <img className='logof' src={logo_empresa} alt='logo'></img>
+      </div>
     </footer>
   );
-});
-
-
-
+};
 
 export default Footer;
